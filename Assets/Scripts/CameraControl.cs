3910e508d175d22092acd567772d1d50b56d2157
Gokul Sudheesh <gokulsudheesh1@gmail.com>
2021-07-06 22:22:16 +0400

    added cam switching

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraControl : MonoBehaviour
+{
+    GameObject player;
+    GameObject vitualCam;
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+    public float fpCamHeight = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    public bool fpCam;
+    CinemachineBrain tpVcam;
+    // Awake is called when the script instance is being loaded.
+    private void Awake()
+    {
+        player = GameObject.Find("Player");
+        fpCam = true;
+        // Get Cinemachine instance
+        tpVcam = GetComponent<CinemachineBrain>();
+        tpVcam.enabled = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            fpCam = !fpCam;
+            tpVcam.enabled = !tpVcam.enabled;
+        }
+    }
+    void LateUpdate()
+    {
+        if (fpCam)
+        {
+            // Camera Rotation: https://gamedev.stackexchange.com/questions/104693/how-to-use-input-getaxismouse-x-y-to-rotate-the-camera
+            yaw += speedH * Input.GetAxis("Mouse X");
+            pitch -= speedV * Input.GetAxis("Mouse Y");
+
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+            player.transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y + fpCamHeight, player.transform.position.z);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,24 +7,41 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float rotationSpeed;
 
+    CameraControl camera;
+    // Awake is called when the script instance is being loaded.
+    private void Awake()
+    {
+        GameObject camObject = GameObject.Find("Main Camera");
+        camera = camObject.GetComponent<CameraControl>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");//-1 or 1 value is inputted to the horizontal axis. By default left and right arrow keys.
         float verticalInput = Input.GetAxis("Vertical"); //Mapped to the up and down arrow key
-        
-        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);//Three dimensional vector required for the object to move
-        movementDirection.Normalize();//Normalizes the vector and sets magnitude to one
-        
-        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);//Changing position of the object based on the movement direction. The transalte method helps move it in the direction that we want
-        
-        if (movementDirection != Vector3.zero)
+
+        if (camera.fpCam)
         {
-               //Quaternion is a representation of a vector and a rotation around that vector
-               
-        	Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);//Creates a rotation looking in the desired direction. (x, y)
-        	
-        	transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);//Rotate the character to the quaternion variable
+            // First person player movement
+            transform.Translate(Vector3.forward * speed * verticalInput * Time.deltaTime); // +1 forward -1 backward
+            transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime); // +1 right -1 left
+        }
+        else
+        {
+            Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);//Three dimensional vector required for the object to move
+            movementDirection.Normalize();//Normalizes the vector and sets magnitude to one
+
+            transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);//Changing position of the object based on the movement direction. The transalte method helps move it in the direction that we want
+
+            if (movementDirection != Vector3.zero)
+            {
+                //Quaternion is a representation of a vector and a rotation around that vector
+
+                Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);//Creates a rotation looking in the desired direction. (x, y)
+
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);//Rotate the character to the quaternion variable
+            }
         }
     }
 }
