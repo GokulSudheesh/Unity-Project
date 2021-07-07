e7d4529add2c4072f87418483be46620f92f55b6
Yashika Jamwal <yashikajamwal.312001@gmail.com>
2021-07-07 12:14:22 +0400

    Updated

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //*****variables*******
+    
     [SerializeField] private float speed;
     [SerializeField] private float rotationSpeed;
     private Rigidbody rb;
     [SerializeField] private float jumpSpeed; //Calculate the jump speed
     private bool onGround;
+    
     CameraControl camera;
-
+    
+    private float coolDown = 6;
+    [SerializeField] private float coolDownTimer;
+    [SerializeField] private float sprintingTimer;
+    private float sprintTime = 6;
+    public Text playerStat;
+    private float countDown = 5;
+    
+    //**********************
+    
+    
     // Awake is called when the script instance is being loaded.
     private void Awake()
     {
@@ -20,16 +34,14 @@ public class PlayerMovement : MonoBehaviour
         GameObject camObject = GameObject.Find("Main Camera");
         camera = camObject.GetComponent<CameraControl>();
     }
-    void Start()
-    {
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");//-1 or 1 value is inputted to the horizontal axis. By default left and right arrow keys.
         float verticalInput = Input.GetAxis("Vertical"); //Mapped to the up and down arrow key
-
+	
+        //Checking if the object is on the ground to make it jump on the ground and not in the air
         if (onGround)
         {
 
@@ -43,32 +55,65 @@ public class PlayerMovement : MonoBehaviour
                 onGround = false;
             }
         }
+        
+        //****First person camera*****
         if(camera.fpCam)
         {
             // First person player movement
             transform.Translate(Vector3.forward * speed * verticalInput * Time.deltaTime); // +1 forward -1 backward
             transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime); // +1 right -1 left
         }
-        else
-        {
-            // Third person player movement
-            Vector3 movementDirection = new Vector3(verticalInput, 0, -horizontalInput);//Three dimensional vector required for the object to move
-            movementDirection.Normalize();//Normalizes the vector and sets magnitude to one
+        //****************************
+        
+        
+        else{
+        // Third person player movement
+        Vector3 movementDirection = new Vector3(verticalInput, 0, -horizontalInput);//Three dimensional vector required for the object to move
+        movementDirection.Normalize();//Normalizes the vector and sets magnitude to one
 
-            rb.AddForce(movementDirection * speed);
+        rb.AddForce(movementDirection * speed);
 
-            transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);//Changing position of the object based on the movement direction. The transalte method helps move it in the direction that we want
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);//Changing position of the object based on the movement direction. The transalte method helps move it in the direction that we want
                                                                                          //Time.deltaTime is the amount of seconds that have passed since the last frame
 
-            if (movementDirection != Vector3.zero)
-            {
+         if (movementDirection != Vector3.zero)
+         {
                 //Quaternion is a representation of a vector and a rotation around that vector
 
                 Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);//Creates a rotation looking in the desired direction. (x, y)
 
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);//Rotate the character to the quaternion variable
-            }
+         }
+            
+            //Simple code to make the character sprint
+        if (Input.GetKey(KeyCode.Z)){
+       
+        		if (sprintingTimer > 0 && coolDownTimer == coolDown){
+        			transform.Translate(movementDirection * (speed*2) * Time.deltaTime, Space.World);
+        			sprintingTimer -= Time.deltaTime;
+        		}		
         }
+        
+        
+        
+        if (sprintingTimer < 0){
+        		
+		coolDownTimer -= Time.deltaTime;
+        
+        }
+        
+        if (coolDownTimer < 0)
+        {
+        	coolDownTimer = coolDown;
+        	sprintingTimer = sprintTime;
+        }
+      
+        //End of Character sprint code
+        //Need to find way to add stamina at any moment
+        
+    		
+    	playerStat.text = sprintingTimer.ToString();
+    	}
         
     }
 
@@ -80,4 +125,5 @@ public class PlayerMovement : MonoBehaviour
             onGround = true;
         }
     }
+    
 }
