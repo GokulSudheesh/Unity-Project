fd21fe8b91b28fc2543485b73b1c6807d9780c0c
Gokul Sudheesh <gokulsudheesh1@gmail.com>
2021-07-07 14:54:23 +0400

    refactored code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float rotationSpeed;
 
     CameraControl camera;
+
+    Vector3 movementDirection;
+    Vector3 movementDirectionH;
     // Awake is called when the script instance is being loaded.
     private void Awake()
     {
@@ -20,19 +23,21 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");//-1 or 1 value is inputted to the horizontal axis. By default left and right arrow keys.
         float verticalInput = Input.GetAxis("Vertical"); //Mapped to the up and down arrow key
-
+        var space = Space.Self;
         if (camera.fpCam)
         {
+            space = Space.Self;
             // First person player movement
-            transform.Translate(Vector3.forward * speed * verticalInput * Time.deltaTime); // +1 forward -1 backward
-            transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime); // +1 right -1 left
+            movementDirection = Vector3.forward * verticalInput;
+            movementDirectionH = Vector3.right * horizontalInput;
         }
         else
         {
-            Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);//Three dimensional vector required for the object to move
+            space = Space.World;
+            // Third person player movement
+            movementDirection = new Vector3(horizontalInput, 0, verticalInput);//Three dimensional vector required for the object to move
             movementDirection.Normalize();//Normalizes the vector and sets magnitude to one
-
-            transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);//Changing position of the object based on the movement direction. The transalte method helps move it in the direction that we want
+            movementDirectionH = new Vector3(0, 0, 0);
 
             if (movementDirection != Vector3.zero)
             {
@@ -43,5 +48,7 @@ public class PlayerMovement : MonoBehaviour
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);//Rotate the character to the quaternion variable
             }
         }
+        transform.Translate(movementDirection * speed * Time.deltaTime, space); // +1 forward -1 backward
+        transform.Translate(movementDirectionH * (speed - 1) * Time.deltaTime); // +1 right -1 left
     }
 }
